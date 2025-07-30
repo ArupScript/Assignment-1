22e1fb3231e8655f11468e08289281d0257ed09e
ArupScript <arupm22@gmail.com>
2025-07-30 19:31:41 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,11 @@
+#Task 1
+a=int(input('Enter the first number: '))
+b=int(input('Enter the second number: '))
+A=a+b
+S=a-b
+M=a*b
+D=a/b
+print('\nAddition: ',A)
+print('Substraction: ',S)
+print('Multiplication: ',M)
+print('Division: ',D)
---
+++ Task 2.py
@@ -0,0 +1,4 @@
+first=input('Enter your first name: ')
+last=input('Enter your second name: ')
+full= first + " " + last
+print('\nHello, ' + full + " welcome to the python world.")
